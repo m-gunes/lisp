9565afb9519651adcf3539f47b5b714ff20fd88d
Mustafa Güneş <mustafa.gunes@nesine.com>
2017-11-26 19:30:14 +0200

    lisp function

--- lisp_5_function.rkt
+++ lisp_5_function.rkt
@@ -1,24 +1,32 @@
 #lang racket
 
 
+(define (ikimisli n) (* n 2))
+(ikimisli 100)
+
+; lisp in diğer dillerderden üstünlüğü
+; bir fonksiyonu fonksiyona parametre olarak verebiliyoruz
+(define (uygula fonksiyon a b)
+  (fonksiyon a b)
+  )
+(uygula + 10 20)
 
 
-
-
-
-
-
-
-
-
-
-
-
-(define
-  (factorial n)
-   (if (= n 0) 1
-   (* n (factorial (- n 1) ) )
-   )
+(define (factorial n)
+  (if (= n 0) 1
+      (* n (factorial(- n 1)))
+  )
 )
-
 (factorial 5)
+
+(define (fibonacci n)
+  (if (< n 2) n
+      (+ (fibonacci(- n 1)) (fibonacci(- n 2)) )
+  )
+)
+(fibonacci 8)
+
+
+
+
+
